c75f316c12c84ba830ab48d53242c8acdd46e04e
Shawn <shawn.harrington2776@gmail.com>
2022-02-03 03:41:13 -0800

    init commit

---
+++ src/Home.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './Home.css';
+
+
+function Home() {
+  return (
+    <div className='home'>
+      <div className='home__container'>
+        <img className='home__image' src='/images/bg2.jpg' alt='bg'/>
+
+        <div className='home__row'>
+
+        </div>
+
+        <div className='home__row'>
+
+        </div>
+
+        <div className='home__row'>
+
+        </div>
+
+      </div>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBqk1n6QSB9jGYYnAUg_fkRPwnr9TKbSxs",
+  authDomain: "amac-ecea1.firebaseapp.com",
+  projectId: "amac-ecea1",
+  storageBucket: "amac-ecea1.appspot.com",
+  messagingSenderId: "244783697112",
+  appId: "1:244783697112:web:a4c3b01a8c78334183dbad",
+  measurementId: "G-YHG07W3YVV"
+};
